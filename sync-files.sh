a595b562cb770797272048bffee5aa338db5019f
FearnDreams <2543917281@qq.com>
2024-11-21 23:30:21 +0800

    更新需求分析文档初稿以及开发计划文档的部分内容

---
+++ sync-files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 同步需求分析文件到 docs 文件夹
+cp "/c/Users/86177/Documents/ObsidianVault/毕设/文档/1 需求与系统设计/需求分析.md" ./docs/需求分析.md
+cp "/c/Users/86177/Documents/ObsidianVault/毕设/文档/1 需求与系统设计/系统架构设计.md" ./docs/系统架构设计.md
+cp "/c/Users/86177/Documents/ObsidianVault/毕设/文档/2 开发与实施/开发计划.md" ./docs/开发计划.md
+cp "/c/Users/86177/Documents/ObsidianVault/毕设/文档/1 需求与系统设计/模块设计.md" ./docs/模块设计.md
+echo "同步完成！文件已更新到 docs 文件夹。"
+
